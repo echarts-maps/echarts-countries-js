417cd1dcc9a0ad1a0b61e2026ba5bb07612288f4
chfw <wangc_2011@hotmail.com>
2018-01-13 00:16:29 +0000

    :newspaper: add loaded message

---
+++ echarts-countries-js/index.js
@@ -0,0 +1,9 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var version = '1.0.2';
+    function load_ipython_extension() {
+        console.log("echarts-countries-js " + version + " has been loaded");
+    }
+    exports.load_ipython_extension = load_ipython_extension;
+});
